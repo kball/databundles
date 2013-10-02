a3cf2ab0ae8fde2778483ec8d2fff7b5fa369ce3
Eric Busboom <eric@clarinova.com>
2013-10-01 18:02:37 -0700

    Error in field name

--- databundles/partition/sqlite.py
+++ databundles/partition/sqlite.py
@@ -214,15 +214,11 @@ class SqlitePartition(PartitionBase):
         self.record.count = s.execute("SELECT COUNT(*) FROM {}".format(t.name)).scalar()
         self.record.min_key = s.execute("SELECT MIN({}) FROM {}".format(t.primary_key.name,t.name)).scalar()
         self.record.max_key = s.execute("SELECT MAX({}) FROM {}".format(t.primary_key.name,t.name)).scalar()
-        
-        print self.record.count, t.name
-
+ 
         s = self.bundle.database.session
         s.merge(self.record)
         s.commit()
 
-
-        
 
 
     def __repr__(self):
