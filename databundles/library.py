d9d1a13bd6351e062893a88cec91a18d5ddd4c2e
Eric Busboom <eric@clarinova.com>
2013-11-19 15:23:16 -0800

    get partitions when returning csv

--- databundles/library.py
+++ databundles/library.py
@@ -1605,6 +1605,8 @@ class Library(object):
                 try:
                     
                     if force or p.database.is_empty():
+                        self.logger("_get_partition deleting {} before fetch. force={}, is_empty={}"
+                                    .format((p.database.path, force, p.database.is_empty()))
                         os.remove(p.database.path)
                     
                     self._get_remote_partition(r,partition, cb=cb)
